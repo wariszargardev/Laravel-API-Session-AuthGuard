b698fac0a180f0aca1e111e6b159ffc84760e970
wariszargardev <wariszargardev@gmail.com>
2022-10-23 11:14:46 +0500

    COnstants setup for our project

---
+++ app/Constants/AuthRedirectionConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants;
+
+final class AuthRedirectionConstants
+{
+    const ADMIN_DASHBOARD = '/admin/dashboard';
+    const ADMIN_LOGIN = '/admin/login';
+    const USER_DASHBOARD = '/user/dashboard';
+    const USER_LOGIN = '/login';
+}
---
+++ app/Constants/MediaLibraryConstants.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Constants;
+
+final class MediaLibraryConstants
+{
+    const USER_PROFILE_IMAGE = 'images/user_profile';
+}
